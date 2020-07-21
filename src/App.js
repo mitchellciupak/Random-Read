59aa8fab1ad75fe3cc65b17b0897b74648585c20
mitchellciupak <ciupak@purdue.edu>
2020-07-21 17:15:17 -0500

    Adding Request to Console

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'request-promise'
+import 'xml2js'
 
-const auth_token = process.env.REACT_APP_AUTH_TOKEN;
-const secret_auth_token = process.env.REACT_APP_SECRET_AUTH_TOKEN;
+// const auth_token = process.env.REACT_APP_AUTH_TOKEN;
+// const secret_auth_token = process.env.REACT_APP_SECRET_AUTH_TOKEN;
 
 var request = require('request-promise');
-require('dotnev'),config();
+var xml2js = require('xml2js');
+
+require('dotenv').config();
 
 let options = {
-  method: 'GET',
-  uri: 'https://www.goodreads.com/review/list/${process.env.USER_ID}.xml`,
-  qs: {
-      key: process.env.REACT_APP_AUTH_TOKEN,
-      v: process.env.VERSION,
-      shelf: process.env.SHELF,
-      per_page: process.env.PER_PAGE
-  }
+    method: 'GET',
+    uri: `https://www.goodreads.com/review/list/${process.env.USER_ID}.xml`,
+    qs: {
+        key: process.env.REACT_APP_AUTH_TOKEN,
+        v: process.env.VERSION,
+        shelf: process.env.SHELF,
+        per_page: process.env.PER_PAGE
+    }
 }
 
 request(options).then((shelf) => {
-    console.log(shelf);
-}).catch(err => console.error(err));
+    xml2js.parseString(shelf, function (err, result) {
+        let books = result['GoodreadsResponse']['reviews'][0]['review'];
+        let index = Math.floor(Math.random() * books.length);
+        let title = books[index]['book'][0]['title'][0];
+        console.log(title);
+    });
+
+}).catch(err => console.error(err));;
 
 function App() {
   return (
